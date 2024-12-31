84b7ab97c41426e1be11b1af0743f191d277e2d0
Jerry <azhagirishankar@appstronauts.in>
2024-12-31 16:04:02 +0530

    #3 Refactor changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,11 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Jerry',
-            'email' => 'jerry@gmail.com',
-            'password' => Hash::make('password')
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin123')
         ]);
     }
 }
